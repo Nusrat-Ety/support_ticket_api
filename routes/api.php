fe79ea881078bb95a1ef0f781d3c883339a987a2
NusratJahanEty <nusratety96@gmail.com>
2024-05-26 23:46:47 +0600

    database & seeder created for ticket,testing ticket data

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/login', [AuthController::class, 'login']);
+Route::get('/ticket', function() {
+    return Ticket::all();
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
