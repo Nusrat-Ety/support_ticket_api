6efd32b9ab098e54df764e13a1b5d9a74765edeb
NusratJahanEty <nusratety96@gmail.com>
2024-05-29 22:34:35 +0600

    Api versioning

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/login', [AuthController::class, 'login']);
+Route::apiResource('tickets', TicketController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
