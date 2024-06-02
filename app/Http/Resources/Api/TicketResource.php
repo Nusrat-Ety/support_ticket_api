eaf8e3e786ab45b3aa1a6ec8d014c2e13e6991d0
NusratJahanEty <nusratety96@gmail.com>
2024-06-02 21:59:12 +0600

    Designing response payloads

---
+++ app/Http/Resources/Api/TicketResource.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketResource extends JsonResource
+{
+    /* For changing wrapper name of collection
+    public static $wrap = "Ticket"; */
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'Tickets',
+            'id' => $this->id,
+            'attributes' => [
+                'title' => $this->title,
+                'status' => $this->status,
+                'description' => $this->description,
+                'createdAt' => $this->created_at,
+                'updatedAt' => $this->updated_at
+            ],
+            'relationships' => [
+                'author' => [
+                    'data' => [
+                        'type' => 'user',
+                        'id' => $this->user_id
+                    ],
+                    'links' => [
+                        'self' => 'undone'
+                    ]
+                ],
+
+            ],
+            'links' => [
+                'self' => route('tickets.show', $this->id)
+            ]
+        ];
+    }
+}
