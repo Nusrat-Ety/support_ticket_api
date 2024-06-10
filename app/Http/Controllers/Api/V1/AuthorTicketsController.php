cf01550a895ae077ee2b22cdb2839f39a2afe297
NusratJahanEty <nusratety96@gmail.com>
2024-06-11 00:22:46 +0600

    Creating Resources with Post Requests

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\Api\TicketResource;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
@@ -15,5 +16,21 @@ class AuthorTicketsController extends Controller
         // Retrieve tickets associated with the specified author ID & apply any filters provided.
         
         return TicketResource::collection(Ticket::where('user_id', $author_id)->filter($filters)->paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTicketRequest $request, $author_id)
+    {
+
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $author_id,
+        ];
+
+        return new TicketResource(Ticket::create($model));
     }
 }
--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Ticket;
-use App\Http\Requests\StoreTicketRequest;
+use App\Models\User;
+use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\UpdateTicketRequest;
 use App\Http\Resources\Api\TicketResource;
 use App\Http\Controllers\Api\V1\ApiController;
 use App\Http\Filters\V1\TicketFilter;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class TicketController extends ApiController
 {
@@ -24,7 +26,24 @@ class TicketController extends ApiController
      */
     public function store(StoreTicketRequest $request)
     {
-        //
+        try {
+            
+            $user = User::findOrFail($request->input('data.relationships.author.data.id'));
+
+        } catch (ModelNotFoundException $exception) {
+            return $this->ok('User not found.', [
+                'error' => 'The provided user id does not exists'
+            ]);
+        }
+
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $request->input('data.relationships.author.data.id'),
+        ];
+
+        return new TicketResource(Ticket::create($model));
     }
 
     /**
